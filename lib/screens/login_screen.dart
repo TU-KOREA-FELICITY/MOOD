670c458401d275f0f1d7d9e82366cea8fe1cf871
INSUN <emilyinsun0220@naver.com>
2025-01-07 20:54:21 +0900

    commit3

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,18 +5,55 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('로그인'),
+        title: Text(
+          '로그인',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       body: Column(
         children: [
           Expanded(
-            child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('운전자 인식중'),
-                  SizedBox(height: 20),
-                  // 여기에 카메라API 추가
+                  SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '운전자 인식중',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  SizedBox(
+                    width: 20,
+                    height: 20,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+                      strokeWidth: 2,
+                    ),
+                  ),
+                ],
+              ),
+                  SizedBox(height: 30),
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.orange, width: 3),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text('카메라 화면 영역'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 100),
                 ],
               ),
             ),
@@ -24,7 +61,7 @@ class LoginScreen extends StatelessWidget {
           Align(
             alignment: Alignment.bottomRight,
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.only(right: 30.0, bottom: 150.0),
               child: GestureDetector(
                 onTap: () {
                   Navigator.pushNamed(context, '/signup');
@@ -32,6 +69,7 @@ class LoginScreen extends StatelessWidget {
                 child: Text(
                   '회원가입',
                   style: TextStyle(
+                    fontSize: 18.0,
                     decoration: TextDecoration.underline,
                   ),
                 ),
--- lib/screens/music_preference_screen.dart
+++ lib/screens/music_preference_screen.dart
@@ -7,61 +7,108 @@ class MusicPreferenceScreen extends StatefulWidget {
 
 class _MusicPreferenceScreenState extends State<MusicPreferenceScreen> {
   final List<String> genres = [
-    '국내발라드', '국내 댄스와 일렉', '국내 힙합', '해외 알앤비', 'OST/BGM', '국내 인디',
-    '해외팝', '국내 알랜비', '트로트', '해외 힙합', '키즈', '클래식', '재즈',
-    '국내 포크/블루스', '해외 일레트로닉', '해외 메탈', '맘/태교', '월드뮤직',
-    '종교음악', '뉴에이지', '국내 팝/어쿠스틱', '국내 록/메탈', '해외 록', 'J-POP',
-    'CCM', '국악'
+    '국내 발라드', '국내 댄스/일렉', '국내 힙합', '국내 알앤비', '국내 인디', 'OST/BGM',
+    '해외 팝', '해외 알앤비', '해외 힙합', '트로트', '키즈', '클래식', '재즈','맘/태교',
+    '종교 음악', '뉴에이지', '국내 팝/어쿠스틱', '록/메탈', 'J-POP', '국악'
   ];
 
   List<String> selectedGenres = [];
+
+  final List<Color> buttonColors = [
+    Colors.red[100]!, Colors.blue[100]!, Colors.green[100]!, Colors.purple[100]!,
+    Colors.teal[100]!, Colors.pink[100]!, Colors.indigo[100]!, Colors.amber[100]!,
+    Colors.cyan[100]!, Colors.lime[100]!, Colors.brown[100]!, Colors.deepOrange[100]!,
+    Colors.lightBlue[100]!, Colors.lightGreen[100]!, Colors.deepPurple[100]!, Colors.yellow[100]!,
+    Colors.blueGrey[100]!, Colors.grey[300]!, Colors.amber[100]!, Colors.red[100]!
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('음악 취향 선택'),
+        title: Text(
+          '선호하는 장르 선택',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        titleSpacing: 0,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          SizedBox(height: 20),
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
             child: Text(
-              '선호하는 음악 취향을 모두 선택하세요',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              '선호하는 음악 장르를 모두 선택하시오',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: genres.length,
-              itemBuilder: (context, index) {
-                final genre = genres[index];
-                return CheckboxListTile(
-                  title: Text(genre),
-                  value: selectedGenres.contains(genre),
-                  onChanged: (bool? value) {
-                    setState(() {
-                      if (value!) {
-                        selectedGenres.add(genre);
-                      } else {
-                        selectedGenres.remove(genre);
-                      }
-                    });
-                  },
-                );
-              },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20.0,
+                  mainAxisSpacing: 20.0,
+                  childAspectRatio: 3.0,
+                ),
+                itemCount: genres.length,
+                itemBuilder: (context, index) {
+                  final genre = genres[index];
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (selectedGenres.contains(genre)) {
+                          selectedGenres.remove(genre);
+                        } else {
+                          selectedGenres.add(genre);
+                        }
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: selectedGenres.contains(genre)
+                            ? Colors.orange
+                            : buttonColors[index % buttonColors.length],
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        genre,
+                        style: TextStyle(
+                          color: selectedGenres.contains(genre) ? Colors.white : Colors.black,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: selectedGenres.isNotEmpty
-                  ? () {
-                // 회원가입 완료 로직 구현
-                Navigator.pushReplacementNamed(context, '/main');
-              }
-                  : null,
-              child: Text('회원가입 완료'),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 32.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: selectedGenres.isNotEmpty
+                      ? () {
+                    Navigator.pushReplacementNamed(context, '/main');
+                  }
+                      : null,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFEB9A1F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text('회원가입 완료', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,
+                    color: Colors.black,)),
+                ),
+              ),
             ),
           ),
         ],
