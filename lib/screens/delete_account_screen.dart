b916d257755046cdf4207057bb80c5e76102248f
INSUN <emilyinsun0220@naver.com>
2025-01-15 18:31:13 +0900

    commit13

--- lib/screens/delete_account_screen.dart
+++ lib/screens/delete_account_screen.dart
@@ -2,31 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:mood/screens/login_screen.dart';
 
 class DeleteAccountScreen extends StatelessWidget {
+  final String userName = 'IN SUN';
+  final String carModel = 'Mercedes-Benz AMG G63';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('회원탈퇴'),
+        centerTitle: false,
+        title: Text(
+          '회원탈퇴',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Icon(Icons.sentiment_dissatisfied, size: 100),
+            SizedBox(height: 150),
+            Icon(Icons.delete_outline, size: 100),
             SizedBox(height: 20),
-            Text('사용자 이름', style: TextStyle(fontSize: 24)),
-            Text('사용자 차종'),
-            SizedBox(height: 40),
-            ElevatedButton(
-              child: Text('회원 탈퇴하기'),
-              onPressed: () {
-                // 회원 탈퇴 로직
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
-                      (route) => false,
-                );
-              },
+            Text('이름 : $userName',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Text('차종 : $carModel',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 300),
+            Text('회원을 탈퇴 하시겠습니까?',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 110,
+                  height: 50,
+                  child: ElevatedButton(
+                    child: Text(
+                      'NO',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                    ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  ),
+                ),
+                ),
+                SizedBox(
+                  width: 110,
+                  height: 50,
+                  child: ElevatedButton(
+                    child: Text(
+                      'YES',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                    ),
+                  onPressed: () {
+                    // 회원 탈퇴 로직
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                          (route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF0126FA),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  ),
+                ),
+                ),
+              ],
             ),
           ],
         ),
