af24c408c831ab164649ede3889f831ea3871e27
INSUN <emilyinsun0220@naver.com>
2025-01-24 12:34:27 +0900

    commit30

--- lib/screens/start_screens/signup_completion_screen.dart
+++ lib/screens/start_screens/signup_completion_screen.dart
@@ -14,27 +14,27 @@ class SignUpCompletionScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-            SizedBox(height: 90),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.black,
-                ),
-                children: <TextSpan>[
-                  TextSpan(text: 'Welcome to '),
-                  TextSpan(
-                    text: 'MOOD',
-                    style: TextStyle(color: Color(0xFF0126FA)),
+              SizedBox(height: 120),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
                   ),
-                  TextSpan(text: '!'),
-                ],
+                  children: <TextSpan>[
+                    TextSpan(text: 'Welcome to '),
+                    TextSpan(
+                      text: 'MOOD',
+                      style: TextStyle(color: Color(0xFF0126FA)),
+                    ),
+                    TextSpan(text: '!'),
+                  ],
+                ),
               ),
-            ),
-              const SizedBox(height: 40),
+              const SizedBox(height: 50),
               const Icon(
                 Icons.check_circle,
                 color: Colors.black,
@@ -43,20 +43,36 @@ class SignUpCompletionScreen extends StatelessWidget {
               const SizedBox(height: 20),
               const Text(
                 '회원가입 완료',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 40),
               Text(
                 '$userName님의 회원가입이\n성공적으로 완료되었습니다.',
                 textAlign: TextAlign.center,
                 style: const TextStyle(fontSize: 16),
               ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushReplacementNamed('/home');
-                },
-                child: const Text('시작하기'),
+              SizedBox(height: 300),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white, backgroundColor: Color(0xFF0126FA),
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    '시작하기',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
